0960b7d13e1457bcc8334a43505b42183063b1b5
macfarley90 <therealmccoyster@gmail.com>
2025-04-04 09:01:58 -0400

    before merge

--- server.js
+++ server.js
@@ -66,7 +66,8 @@ mongoose.connection.on("connected", () => {
   });
 
 
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+//connection port
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log('Listening on port ', port);
 });
